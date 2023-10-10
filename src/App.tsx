04fc7a4f9d3d2281e087e98e30c5898dc7ccd70a
vladk7072 <vladk7072@gmail.com>
2023-10-10 17:27:02 +0300

    safe

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { MainMap } from "./components/MainMap";
+import { Kisska_ip } from './components/Kisska_ip';
 
 
 export default function App() {
   return (
     <section className="main">
+      <Kisska_ip />
       <MainMap isDarkTheme={false} />
     </section>
   );
