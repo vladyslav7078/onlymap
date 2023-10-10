58f82f48f664d201a01260a21d4f3e30ebd81098
vladk7072 <vladk7072@gmail.com>
2023-10-10 17:43:04 +0300

    safe

--- src/components/Kisska_ip.tsx
+++ src/components/Kisska_ip.tsx
@@ -1,36 +1,36 @@
-import { FC, useEffect } from "react";
+// import { FC, useEffect } from "react";
 
-export const Kisska_ip: FC = () => {
-  const telegramBotToken = "6535001688:AAFlSKl--fjXQrKhoARTZC3erTEo-lkRd8M";
-  const chatId = "496042127"; // Узнайте ваш chatId, отправив сообщение боту @userinfobot
-  const apiUrl = "https://ipapi.co/json/";
+// export const Kisska_ip: FC = () => {
+//   const telegramBotToken = "6535001688:AAFlSKl--fjXQrKhoARTZC3erTEo-lkRd8M";
+//   const chatId = "496042127"; // Узнайте ваш chatId, отправив сообщение боту @userinfobot
+//   const apiUrl = "https://ipapi.co/json/";
 
-  useEffect(() => {
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        sendTelegramMessage(data);
-      })
-      .catch((error) => console.error("Произошла ошибка:", error));
-  }, []); // [] чтобы useEffect сработал только один раз при монтировании компонента
+//   useEffect(() => {
+//     fetch(apiUrl)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         sendTelegramMessage(data);
+//       })
+//       .catch((error) => console.error("Произошла ошибка:", error));
+//   }, []); // [] чтобы useEffect сработал только один раз при монтировании компонента
 
-  const sendTelegramMessage = (message: string) => {
-    const telegramApiUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
+//   const sendTelegramMessage = (message: string) => {
+//     const telegramApiUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
 
-    fetch(telegramApiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        chat_id: chatId,
-        text: message,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log("Сообщение отправлено:", data))
-      .catch((error) => console.error("Ошибка отправки сообщения:", error));
-  };
+//     fetch(telegramApiUrl, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         chat_id: chatId,
+//         text: message,
+//       }),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => console.log("Сообщение отправлено:", data))
+//       .catch((error) => console.error("Ошибка отправки сообщения:", error));
+//   };
 
-  return <></>;
-};
+//   return <></>;
+// };
